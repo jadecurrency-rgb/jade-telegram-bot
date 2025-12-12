b3705f47727186c3ad9897db4812371c05c1842e
jadecurrency-rgb <jadecurrency@gmail.com>
2025-12-11 20:11:40 -0600

    Update server.js
    Fix binding to make server reachable

--- server.js
+++ server.js
@@ -44,6 +44,6 @@ https://jade1.io
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log('Server running on port ' + PORT);
 });
